38c589cba23f0c38d646b41e118b5b45ff4c962e
mojtaba ameli bidar <mojtabaas95@gmail.com>
2022-04-28 10:36:57 +0430

    finishing logic of project

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,22 @@
 const rowNumber = 7;
 const colNumber = 10;
-const minesNumber =10;
+const minesNumber = 15;
 let remainMins = minesNumber;
 
-document.querySelector('.mins-number').innerHTML = minesNumber;
+const logger = (arr) => {
+    let str = '';
+    for (let i = 0; i < arr.length; i++) {
+        str += '| ';
+        for (let j = 0; j < arr[0].length; j++) {
+            str += arr[i][j] + ' | ';
+        }
+        str += '\n';
+    }
+    console.log(str);
+};
+
+
+document.querySelector('.mins-number').innerHTML = remainMins;
 
 // class BoardUnit {
 //     value;
@@ -19,8 +32,8 @@ document.querySelector('.mins-number').innerHTML = minesNumber;
 
 const gameArray = new Array(rowNumber).fill(0).map(() => new Array(colNumber).fill(0));
 
-const gameItemState = new Array(rowNumber).fill(false).map(() => new Array(colNumber).fill(false));
-console.log(gameItemState);
+const gameStateArray = new Array(rowNumber).fill(false).map(() => new Array(colNumber).fill(false));
+// console.log(gameStateArray);
 
 console.log(gameArray);
 
@@ -47,7 +60,7 @@ const randomIndexUpToNumber = (num) => {
     return Math.floor(Math.random() * num);
 };
 
-//  create array for pick mine in location of created in below
+//  create array for pick mines in location of created in below
 let existLocation = [];
 let gameArrayLength = gameArray.length;
 let gameColLength = gameArray[0].length;
@@ -64,12 +77,15 @@ const createExistLocationArr = () => {
 };
 
 const locatedMinesInGameArray = () => {
-    let rndIndex = randomIndexUpToNumber(existLocation.length - 1);
-    let rowGameArray = existLocation[rndIndex][0];
-    let colGameArray = existLocation[rndIndex][1];
-    existLocation.splice(rndIndex, 1);
-    gameArray[rowGameArray][colGameArray] = 'x';
-}
+    createExistLocationArr();
+    for(let i = 0; i < minesNumber; i++) {
+        let rndIndex = randomIndexUpToNumber(existLocation.length - 1);
+        let rowGameArray = existLocation[rndIndex][0];
+        let colGameArray = existLocation[rndIndex][1];
+        existLocation.splice(rndIndex, 1);
+        gameArray[rowGameArray][colGameArray] = 'x';
+    }
+};
 
 const setNumberSurroundOfMines = () => {
     for(let i = 0; i < gameArrayLength; i++) {
@@ -93,14 +109,11 @@ const setNumberSurroundOfMines = () => {
         }
         
     }
-}
+};
 
 const initializeBoard = () => {
     createBoardUI();
-    createExistLocationArr();
-    for(let i = 0; i < minesNumber; i++) {
-        locatedMinesInGameArray();
-    }
+    locatedMinesInGameArray();
     setNumberSurroundOfMines();
 };
 
@@ -111,113 +124,177 @@ initializeBoard();
 //  Add event listener on board item
 const boardItems = document.querySelectorAll('.game-board  div');
 
+disableClickAndContextmenu = (row, col) => {
+    document.getElementById(`${row}-${col}`).removeEventListener('click', itemClickHandler);
+    document.getElementById(`${row}-${col}`).removeEventListener('contextmenu', itemRightClickHandler);
+};
+
+const updateMinesNumberUi = () => {
+    const remainsMinsHeader = document.querySelector('.mins-number');
+    remainsMinsHeader.innerHTML = remainMins;
+};
+
 const gameOver = (itm) => {
-    itm.style.color = 'red';
-    itm.style.backgroundColor = 'orange';
-    itm.style.fontSize = '20px';
-    itm.style.fontWeight = 'bolder';
+    // console.log('you are game over');
+
+    itm.style.color = '#ff0000';
+
     for(const item of boardItems) {
-        item.classList.add('reset-color-items');
+        // delete event listener from all elements when user clicked on any mines
         let itemIndexArray = item.id.split('-');
-        item.textContent = gameArray[itemIndexArray[0]][itemIndexArray[1]];
-        item.removeEventListener('click', itemClickHandler); // delete event listener from all elements when user clicked on any mines
+        disableClickAndContextmenu(itemIndexArray[0], itemIndexArray[1]);  
+        let itemContent = gameArray[itemIndexArray[0]][itemIndexArray[1]];
+        // item.classList.add('reset-color-items');
+        if(itemContent === 'x') {
+            item.innerHTML = 'x';
+        }
     }
 };
 
 const emptyZeroElement = (rowIdx, colIdx) => {
-    // arguments is string and we have to change them to number
+    //  arguments is string and we have to change them to number
     let rIdx = +rowIdx;
     let cIdx = +colIdx;
     gameArray[rIdx][cIdx] = 'r';
     
+    gameStateArray[rIdx][cIdx] = true;   //  Update gameStateArray
+    if(document.getElementById(`${rIdx}-${cIdx}`).innerHTML === 'B') {
+        remainMins++;
+        updateMinesNumberUi();
+    }
+    disableClickAndContextmenu(rIdx,cIdx);
+    
     for(let i = rIdx - 1; i <= rIdx + 1; i++) {
         for(let j = cIdx -1; j <= cIdx + 1; j++) {
-            if(!((i >= 0 && i < gameArrayLength) && (j >= 0 && j < gameColLength))) {
+            if(!((i >= 0 && i < gameArrayLength) && (j >= 0 && j < gameColLength))) {   //  for reliza that item is exist in between arrayLength and arrayColLength
                 continue;
             }
-            //  this if condition meant, if element is number grater than zero, show number to user and continue after steps 
-            if(gameArray[i][j] > 0) {
+            if(gameArray[i][j] > 0) {   //  if element is number grater than zero, show number to user and continue after steps
+                if(document.getElementById(`${i}-${j}`).innerHTML === 'B') {
+                    remainMins++;
+                    updateMinesNumberUi();
+                }
+                gameStateArray[i][j] = true;   //  Update gameStateArray
                 document.getElementById(`${i}-${j}`).textContent = gameArray[i][j];
-                document.getElementById(`${i}-${j}`).removeEventListener('click', itemClickHandler);
-                document.getElementById(`${i}-${j}`).removeEventListener('contextmenu', itemRightClickHandler);
+                disableClickAndContextmenu(i, j);
                 continue;
             }
-            if(gameArray[i][j] === 'r') {
+            if(gameArray[i][j] === 'r') {   //  for same element zero that is clicked
                 document.getElementById(`${i}-${j}`).textContent = '';
                 document.getElementById(`${i}-${j}`).classList.add('game-board-empty-item');
-                document.getElementById(`${i}-${j}`).removeEventListener('click', itemClickHandler);
-                document.getElementById(`${i}-${j}`).removeEventListener('contextmenu', itemRightClickHandler);
                 continue;
             }
-            if(document.getElementById(`${i}-${j}`).innerHTML === '<i class="fa fa-flag"></i>') {
-                remainMins++;
-                updateMinesNumberUi();
-            }
             if(gameArray[i][j] === 0) {
-                document.getElementById(`${i}-${j}`).removeEventListener('click', itemClickHandler);
-                document.getElementById(`${i}-${j}`).removeEventListener('contextmenu', itemRightClickHandler);
                 emptyZeroElement(i, j);
             }
         }
     }
 };
 
-const logger = () => {
-    let str = '';
-    for (let i = 0; i < gameArray.length; i++) {
-        str += '| ';
-        for (let j = 0; j < gameArray[0].length; j++) {
-            str += gameArray[i][j] + ' | ';
+const diagnoseWinningUser = () => {
+    let count = 0;  //  number of items that now is not visible(0 or number or flag(B))
+
+    for(const row of gameStateArray) {
+        for(const col of row) {
+            if(col === false) count++;
         }
-        str += '\n';
     }
-    return str;
+
+    let numberUnvisitedItem = count;
+    let correctLocatedMins;
+
+    let i = 0;
+    for(const row of gameStateArray) {
+        let j = 0
+        for(const state of row) {
+            if(state === 'Bomb' && gameArray[i][j] === 'x') {
+                correctLocatedMins =  true;
+            } else if(state === 'Bomb' && gameArray[i][j] !== 'x') {
+                correctLocatedMins =  false;
+            }
+            j++;
+        }
+        i++;
+    }
+    // console.log(correctLocatedMins);
+
+    if(numberUnvisitedItem <= remainMins && correctLocatedMins) {
+        console.log('show user other bombs');
+        console.log('user win');
+
+        let i = 0;
+        for(const row of gameStateArray) {
+            let j = 0;
+            for (const state of row) {
+                disableClickAndContextmenu(i, j);
+                if(state === false || state === 'Bomb') {
+                    gameStateArray[i][j] = 'Bomb';
+                    document.getElementById(`${i}-${j}`).innerHTML = 'Y';
+                }
+                j++;
+            }
+            i++;
+        }
+
+        console.log(gameStateArray);
+
+        remainMins = 0;
+        updateMinesNumberUi();
+    }
 };
 
 const itemClickHandler = event => {
-    console.log('clicked');
+    // console.log('clicked');
     let itemIndexArray = event.currentTarget.id.split('-');
-    let itemContent = gameArray[itemIndexArray[0]][itemIndexArray[1]];
+    let rowItemIndex = itemIndexArray[0];
+    let colItemIndex = itemIndexArray[1];
+    let itemContent = gameArray[rowItemIndex][colItemIndex];
     event.currentTarget.textContent = itemContent;
 
     if(itemContent === 'x') {
         gameOver(event.currentTarget);
     } else if(itemContent === 0) {
-        emptyZeroElement(itemIndexArray[0], itemIndexArray[1]);
+        emptyZeroElement(rowItemIndex, colItemIndex);
+        diagnoseWinningUser();
+    } else if(itemContent > 0) {
+        gameStateArray[rowItemIndex][colItemIndex] = true;
+        disableClickAndContextmenu(rowItemIndex, colItemIndex);
+        diagnoseWinningUser();
     }
     
-    console.log(logger());
+    // logger(gameArray);
+    // console.log(gameStateArray);
 };
 
-
-// disable the right click on the page
-window.addEventListener('contextmenu', event => {
+// disable the right click on the game-board container for handle right click on board items
+document.querySelector('.game-board').addEventListener('contextmenu', event => {
     event.preventDefault();
 });
- 
-const updateMinesNumberUi = () => {
-    const remainsMinsHeader = document.querySelector('.mins-number');
-    remainsMinsHeader.innerHTML = remainMins;
-};
-
 
 const itemRightClickHandler = event => {
-    console.log('right click');
+    // console.log('right click');
+    
     let itemIndexArray = event.currentTarget.id.split('-');
-    let itemContent = gameArray[itemIndexArray[0]][itemIndexArray[1]];  // itemContent show reality of this item
+    let rIdx = itemIndexArray[0];
+    let cIdx = itemIndexArray[1];
+    let itemContent = gameArray[itemIndexArray[0]][itemIndexArray[1]];  // itemContent show reality of this item, that is exist on gameArray
 
     if(event.currentTarget.innerHTML === '' && remainMins > 0) {
-        console.log('1');
-        event.currentTarget.innerHTML = '<i class="fa fa-flag"></i>';
+        event.currentTarget.innerHTML = 'B';
         remainMins--;
-    } else if(event.currentTarget.innerHTML === '<i class="fa fa-flag"></i>') {
-        console.log('2');
+        gameStateArray[rIdx][cIdx] = 'Bomb';
+        document.getElementById(`${rIdx}-${cIdx}`).removeEventListener('click', itemClickHandler);
+    } else if(event.currentTarget.innerHTML === 'B') {
         event.currentTarget.innerHTML = '';
         remainMins++;
+        gameStateArray[rIdx][cIdx] = false;
+        document.getElementById(`${rIdx}-${cIdx}`).addEventListener('click', itemClickHandler);
     }
     
-    console.log(remainMins);
     updateMinesNumberUi();
+    // console.log(remainMins);
+    // console.log('bad az right click shodan');
+    // console.log(gameStateArray);
 };
 
 for(const item of boardItems) {
